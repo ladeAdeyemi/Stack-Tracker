fd5dbd6197c24b8689daa6614bb53c8dbd0da025
Ololade Adeyemi <ssdigital013@gmail.com>
2021-03-08 17:25:32 +0100

    Added functionality that allows the user to remove any added technology by clicking on it. The counter decrements in response.

--- javascript/dom.js
+++ javascript/dom.js
@@ -17,28 +17,46 @@ const input = document.querySelector("#input");
 function createListItem (e) {
 
     e.preventDefault();
-    // console.log(e.target);
-    if (input.value === " ") return;
-    let listItem = document.createElement("li");
-    listItem.className = 'li-styles';
-    listItem.textContent = input.value;
-    listItem.classList.add("inline-block");
-    ul.appendChild(listItem);
-    input.value = " ";
-    incrementCounter();
-    showSaveButton();
 
+    if (input.value === "") {
+
+        return;
+
+    } else {
+
+        let listItem = document.createElement("li");
+        listItem.classList.add("inline-block", "li-styles");
+        listItem.addEventListener('click', deleteTech);
+        listItem.textContent = input.value;
+        ul.appendChild(listItem);
+        input.value = "";
+        incrementCounter();
+        showSaveButton();
+    }
+
+}
+
+function deleteTech (e) {
+      ul.removeChild(e.target);
+      decrementCounter();
 }
 
 function showSaveButton () {
     saveBtn.classList.remove('hide');
     saveBtn.classList.add('show');
+    saveBtn.style.backgroundColor = "black";
 }
 
 function incrementCounter () {
     techCounter++;
     divCounter.textContent = techCounter;
 }
+
+function decrementCounter() {
+  techCounter--;
+  divCounter.textContent = techCounter;
+}
+
 
 // function saveStack () {
 //
@@ -48,3 +66,10 @@ function incrementCounter () {
 
 btn.addEventListener("click", createListItem);
 // saveBtn.addEventListener("click", saveStack);
+
+
+// YOUR JAVASCRIPT FUNDAMENTALS ARE WEAK!
+
+// OPERATORS (MATHEMATICAL, LOGICAL, COMPARISON, ASSIGNMENT), ARRAY METHODS, OBJECT METHODS (INCLUDING THE PARAMETERS THEY ACCEPT), DATA TYPES (BOOLEANS, NULL, UNDEFINED)
+
+// GO BACK AND LEARN ABOUT THESE THINGS AGAIN! EXPERIMENT WITH HOW THEY CAN BE USED IN YOUR PROGRAM I.E. IMAGINE WHAT YOU COULD TELL THE COMPUTER TO DO BY USING THESE THINGS!
