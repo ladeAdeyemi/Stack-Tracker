f0146cb5b61bd0e847c287911d952c762f603106
Ololade Adeyemi <ssdigital013@gmail.com>
2021-03-10 10:04:07 +0100

    Added functionality around the save button: gave it a transition effect so that it appears when users enter a technology into the form. Also added functionality for what happens when the user clicks the save button (all entries cleared from tech display and pushed to an array)

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,9 +1,5 @@
-/* GLOBAL VARIABLES */
-
-let techCounter = 0; // (Displays the number of technologies being tracked by the user)
-
-
 /* DOM SELECTIONS */
+
 
 const btn = document.querySelector("#btn");
 const saveBtn = document.querySelector("#save-btn");
@@ -11,9 +7,23 @@ const ul = document.querySelector("#ul");
 const divCounter = document.querySelector("#counter");
 const input = document.querySelector("#input");
 
+
+/* GLOBAL VARIABLES */
+
+
+let techCounter = 0; // (Displays the number of technologies being tracked by the user)
+let savedStack = []; // (Collects the technologies entered by the user when the save button is pressed)
+
+
+/* EVENT LISTENERS */
+
+
+btn.addEventListener("click", createListItem);
+saveBtn.addEventListener("click", saveStack);
+
+
 /* FUNCTIONS */
 
-// Adds technology submitted in frontend field as a list item to the Ul and records the number of technologies entered
 
 function createListItem (e) {
 
@@ -27,7 +37,7 @@ function createListItem (e) {
 
         let listItem = document.createElement("li");
         listItem.classList.add("inline-block", "li-styles");
-        listItem.addEventListener('click', deleteTech);
+        listItem.addEventListener('click', deleteListItem);
         // listItem.addEventListener('mouseover', showDelete);
         // listItem.addEventListener('mouseout', revert);
         listItem.textContent = input.value;
@@ -35,15 +45,83 @@ function createListItem (e) {
         input.value = "";
         incrementCounter();
         showSaveButton();
+
     }
 
 }
 
-function deleteTech (e) {
-      ul.removeChild(e.target);
-      decrementCounter();
-      hideSaveButton();
+function deleteListItem (e) {
+
+    ul.removeChild(e.target);
+    decrementCounter();
+    hideSaveButton();
+
 }
+
+function showSaveButton () {
+
+    saveBtn.classList.remove('hide');
+    saveBtn.classList.add('show');
+    saveBtn.style.backgroundColor = "black";
+
+}
+
+function hideSaveButton() {
+
+     if (divCounter.textContent === "0" || divCounter.textContent === "Stack Saved.") {
+
+         saveBtn.className = 'hide';
+
+     }
+
+}
+
+function incrementCounter () {
+
+    techCounter++;
+    divCounter.textContent = techCounter;
+
+}
+
+function decrementCounter() {
+
+    techCounter--;
+    divCounter.textContent = techCounter;
+
+}
+
+function saveStack (e) {
+
+    e.preventDefault();
+    let stack = ul.childNodes;
+    stack.forEach((node) => savedStack.push(node.textContent));
+    console.log(savedStack);
+    ul.innerHTML = "";
+    // showStackSavedModal();
+    divCounter.style.fontSize = "50px";
+    divCounter.innerText = "Stack Saved."
+    hideSaveButton();
+    return;
+
+}
+
+
+/*
+
+function showStackSavedModal () {
+
+
+
+}
+
+*/
+
+/*
+
+ NOTE: When you sort out the above functionality (i.e. divCounter.innerText)
+ in a modal, don't forget to change the "or" condition in hideSaveButton()
+
+*/
 
 // function showDelete() {
 //     listItem.innerText = "X";
@@ -52,47 +130,3 @@ function deleteTech (e) {
 // function revert() {
 //   listItem.innerText = input.value;
 // }
-
-function showSaveButton () {
-
-      saveBtn.classList.remove('hide');
-      saveBtn.classList.add('show');
-      saveBtn.style.backgroundColor = "black";
-
-}
-
-function hideSaveButton() {
-
-     if (divCounter.textContent === "0") {
-         saveBtn.className = 'hide';
-     }
-
-}
-
-function incrementCounter () {
-    techCounter++;
-    divCounter.textContent = techCounter;
-}
-
-function decrementCounter() {
-  techCounter--;
-  divCounter.textContent = techCounter;
-}
-
-
-
-// function saveStack () {
-//
-// }
-
-/* EVENT LISTENERS */
-
-btn.addEventListener("click", createListItem);
-// saveBtn.addEventListener("click", saveStack);
-
-
-// YOUR JAVASCRIPT FUNDAMENTALS ARE WEAK!
-
-// OPERATORS (MATHEMATICAL, LOGICAL, COMPARISON, ASSIGNMENT), ARRAY METHODS, OBJECT METHODS (INCLUDING THE PARAMETERS THEY ACCEPT), DATA TYPES (BOOLEANS, NULL, UNDEFINED)
-
-// GO BACK AND LEARN ABOUT THESE THINGS AGAIN! EXPERIMENT WITH HOW THEY CAN BE USED IN YOUR PROGRAM I.E. IMAGINE WHAT YOU COULD TELL THE COMPUTER TO DO BY USING THESE THINGS!
