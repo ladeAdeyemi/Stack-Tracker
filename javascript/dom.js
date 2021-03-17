0c98f6d1ec7d8950a56561fb2f679ea36df95e93
Ololade Adeyemi <ssdigital013@gmail.com>
2021-03-17 20:21:48 +0100

    Added functionality that creates a 'View Stack' button when the user pushes the 'Save' button

--- javascript/dom.js
+++ javascript/dom.js
@@ -4,7 +4,9 @@ const btn = document.querySelector("#btn");
 const saveBtn = document.querySelector("#save-btn");
 const ul = document.querySelector("#ul");
 const divCounter = document.querySelector("#counter");
+const form = document.querySelector("#stack");
 const input = document.querySelector("#input");
+
 
 /* GLOBAL VARIABLES */
 
@@ -28,10 +30,10 @@ function createListItem (e) {
     } else {
 
         const listItem = document.createElement("li");
-        listItem.classList.add("inline-block", "li-styles");
+        listItem.classList.add("li-styles");
         listItem.addEventListener('click', deleteListItem);
-        // listItem.addEventListener('mouseover', showDelete);
-        // listItem.addEventListener('mouseout', revert);
+        // listItem.addEventListener('mouseover', showStrikeThrough);
+        // listItem.addEventListener('mouseout', removeStrikeThrough);
         listItem.textContent = input.value;
         ul.appendChild(listItem);
         input.value = "";
@@ -47,6 +49,20 @@ function deleteListItem (e) {
     ul.removeChild(e.target);
     decrementCounter();
     hideSaveButton();
+
+}
+
+function incrementCounter () {
+
+    techCounter++;
+    divCounter.textContent = techCounter;
+
+}
+
+function decrementCounter() {
+
+    techCounter--;
+    divCounter.textContent = techCounter;
 
 }
 
@@ -68,44 +84,59 @@ function hideSaveButton () {
 
 }
 
-function incrementCounter () {
-
-    techCounter++;
-    divCounter.textContent = techCounter;
-
-}
-
-function decrementCounter() {
-
-    techCounter--;
-    divCounter.textContent = techCounter;
-
-}
-
 function saveStack (e) {
 
     e.preventDefault();
 
-    let savedStack = []; // (Collects the technologies entered by the user when the save button is pressed)
-
     if (divCounter.textContent === "0") {
 
-      return;
+        return;
 
     } else {
 
-      const stack = ul.childNodes;
-      stack.forEach((node) => savedStack.push(node.textContent));
-      console.log(savedStack);
-      ul.innerHTML = "";
-      // showStackSavedModal();
-      divCounter.style.fontSize = "50px";
-      divCounter.innerText = "Stack Saved."
-      hideSaveButton();
-      return;
+        let savedStack = []; // (Collects the technologies entered by the user when the save button is pressed)
+        const stack = ul.childNodes;
+        stack.forEach((node) => savedStack.push(node.textContent));
+        localStorage.stack = savedStack;
+        ul.innerHTML = "";
+        divCounter.style.fontSize = "60px";
+        divCounter.classList.add("counterMargin");
+        divCounter.innerText = "Stack Saved! Never Stop Learning."
+        removeButtons();
+        showViewStackButton();
 
     }
 
+}
+
+function removeButtons () {
+
+    btn.className = "hide";
+    saveBtn.className = "hide";
+    setTimeout(() => {
+      form.removeChild(btn);
+      form.removeChild(saveBtn);
+    }, 1000);
+
+}
+
+function showViewStackButton() {
+
+    let newButton = document.createElement("button");
+    newButton.className = "view-btn";
+    newButton.textContent = "View Stack";
+    newButton.addEventListener('click', viewStack);
+    form.appendChild(newButton);
+
+}
+
+function viewStack (e) {
+
+    e.preventDefault();
+    Array.from(localStorage.stack).forEach((stack)=> {
+    console.log(stack);
+    })
+    
 }
 
 /*
@@ -123,41 +154,11 @@ function showStackSavedModal () {
 
 */
 
-// function showDelete() {
+// function showStrikeThrough() {
 //     listItem.innerText = "X";
+// When the user mousesover a listitem, I want the inner text to have a strikethrough and then when they mouseoff I want the strikethrough gone.
 // }
 //
-// function revert() {
+// function removeStrikeThrough() {
 //   listItem.innerText = input.value;
-// }
-
-// class Stack {
-//
-//     constructor() {
-//
-//     }
-//
-//   sortStack() {
-//     savedStack.sort();
-//   }
-// }
-
-// import { useState } from 'react'
-// import React from 'react'
-//
-// export default function HomePage (props) {
-//
-// const [name, setName] = useState('Olly');
-//
-//     return (
-//       <div className="dr-stone">
-//         <nav className="navBar">
-//           <ul>
-//               <li>About</li>
-//               <li>Contact</li>
-//           </ul>
-//         </nav>
-//       </div>
-//     )
-//
 // }
